e7e09b6b8b682ffaec751e0bf5e082a51c870feb
ttornike1991 <ttornike1991@gmail.com>
2023-03-13 13:41:14 +0000

    Query working implemented

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,65 +1,39 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from lib.db import pool,query_wrap_array
 tracer = trace.get_tracer("home.activities.here")
  
 class HomeActivities:
   def run(logger,cognito_user_id=None):
-    logger.info('home-activities-cloudwatch')
-    with tracer.start_as_current_span("home-page-mock-data"):
-      span = trace.get_current_span()
-      now = datetime.now(timezone.utc).astimezone()
-      span.set_attribute("app.now", now.isoformat())
-       
-      span.set_attribute("user.id", "We have not User id defined in flask")
-      results = [{
-        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'andrew brown',
-        'message': 'cloud is very fun!',
-        'created_at': (now - timedelta(days=2)).isoformat(),
-        'expires_at': (now + timedelta(days=5)).isoformat(),
-        'likes_count': 5,
-        'replies_count': 1,
-        'reposts_count': 0,
-        'replies': [{
-          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-          'handle':  'worf',
-          'message': 'this post has no honor!',
-          'likes_count': 0,
-          'replies_count': 0,
-          'reposts_count': 0,
-          'created_at': (now - timedelta(days=2)).isoformat()
-        }],
-      },
-      {
-        'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-        'handle':  'worf',
-        'message': 'i am out of prune juice',
-        'created_at': (now - timedelta(days=7)).isoformat(),
-        'expires_at': (now + timedelta(days=9)).isoformat(),
-        'likes': 0,
-        'replies': []
-      },
-      {
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'garek',
-        'message': 'my dear doctor, i am just simple tailor',
-        'created_at': (now - timedelta(hours=1)).isoformat(),
-        'expires_at': (now + timedelta(hours=12)).isoformat(),
-        'likes': 0,
-        'replies': []
-      }
-      ]
-      if cognito_user_id != None:
-        extra_crud = {
-          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-          'handle':  'Lore',
-          'message': 'My dear brother, it the humans that are the problem',
-          'created_at': (now - timedelta(hours=1)).isoformat(),
-          'expires_at': (now + timedelta(hours=12)).isoformat(),
-          'likes': 1042,
-          'replies': []
-        }
-        results.insert(0,extra_crud)
-      span.set_attribute("app.result_length", len(results))
+    # logger.info('home-activities-cloudwatch')
+    # with tracer.start_as_current_span("home-page-mock-data"):
+    #   span = trace.get_current_span()
+    # now = datetime.now(timezone.utc).astimezone()
+    #   span.set_attribute("app.now", now.isoformat())  
+    #   span.set_attribute("user.id", "We have not User id defined in flask")
+    
+    sql=query_wrap_array( """
+    SELECT
+        activities.uuid,
+        users.display_name,
+        users.handle,
+        activities.message,
+        activities.replies_count,
+        activities.reposts_count,
+        activities.likes_count,
+        activities.reply_to_activity_uuid,
+        activities.expires_at,
+        activities.created_at
+      FROM public.activities
+      LEFT JOIN public.users ON users.uuid = activities.user_uuid
+      ORDER BY activities.created_at DESC
+    """)
+    with pool.connection() as conn:
+      with conn.cursor() as cur:
+        cur.execute(sql)
+        # this will return a tuple
+        # the first field being the data
+        json = cur.fetchone()
+        print("%%%%%%%%%%%%%%dddddddddddddddddddddd%%%%%%%%%%%%%%55555555555%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%1")
+      return json[0]
       return results
