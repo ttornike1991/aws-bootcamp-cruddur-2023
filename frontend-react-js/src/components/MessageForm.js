bf5b13ded6bc480657f5b8087f4fed19572385b0
ttornike1991 <ttornike1991@gmail.com>
2023-03-23 12:33:09 +0000

    fix some ussues

--- frontend-react-js/src/components/MessageForm.js
+++ frontend-react-js/src/components/MessageForm.js
@@ -3,6 +3,7 @@ import React from "react";
 import process from 'process';
 import { json, useParams } from 'react-router-dom';
 
+
 export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
   const [message, setMessage] = React.useState('');
