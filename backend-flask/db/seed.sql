27487629ee7a431522160e2e2de201532b8773a1
ttornike1991 <ttornike1991@gmail.com>
2023-03-23 09:40:14 +0000

    conversation messages future implemented

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,9 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
-  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+  ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'6def1e2d-4217-44a6-8275-06762a2ca101'),
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'6def1e2d-4217-44a6-8275-06762a2ca101'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'6def1e2d-4217-44a6-8275-06762a2ca101');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
