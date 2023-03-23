a8e38b8063d105a08e2d97aebc7f7f8a7310411d
ttornike1991 <ttornike1991@gmail.com>
2023-03-23 11:46:11 +0000

    implement dynamo dyb in app

--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,49 +1,49 @@
-import './ReplyForm.css';
+import './MessageForm.css';
 import React from "react";
 import process from 'process';
-import {ReactComponent as BombIcon} from './svg/bomb.svg';
+import { json, useParams } from 'react-router-dom';
 
-import ActivityContent  from '../components/ActivityContent';
-
-export default function ReplyForm(props) {
+export default function ActivityForm(props) {
   const [count, setCount] = React.useState(0);
   const [message, setMessage] = React.useState('');
+  const params = useParams();
 
   const classes = []
   classes.push('count')
-  if (240-count < 0){
+  if (1024-count < 0){
     classes.push('err')
   }
 
   const onsubmit = async (event) => {
     event.preventDefault();
     try {
-      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/messages`
+      console.log('onsubmit payload', message)
+      let json = { 'message': message }
+      if (params.handle) {
+        json.handle = params.handle
+      } else {
+        json.message_group_uuid = params.message_group_uuid
+      }
+
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${localStorage.getItem("access_token")}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          message: message
-        }),
+        body: JSON.stringify(json)
       });
       let data = await res.json();
       if (res.status === 200) {
-        // add activity to the feed
-
-        let activities_deep_copy = JSON.parse(JSON.stringify(props.activities))
-        let found_activity = activities_deep_copy.find(function (element) {
-          return element.uuid ===  props.activity.uuid;
-        });
-        found_activity.replies.push(data)
-
-        props.setActivities(activities_deep_copy);
-        // reset and close the form
-        setCount(0)
-        setMessage('')
-        props.setPopped(false)
+        console.log('data:',data)
+        if (data.message_group_uuid) {
+          console.log('redirect to message group')
+          window.location.href = `/messages/${data.message_group_uuid}`
+        } else {
+          props.setMessages(current => [...current,data]);
+        }
       } else {
         console.log(res)
       }
@@ -57,40 +57,21 @@ export default function ReplyForm(props) {
     setMessage(event.target.value);
   }
 
-  let content;
-  if (props.activity){
-    content = <ActivityContent activity={props.activity} />;
-  }
-
-
-  if (props.popped === true) {
-    return (
-      <div className="popup_form_wrap">
-        <div className="popup_form">
-          <div className="popup_heading">
-          </div>
-          <div className="popup_content">
-            <div className="activity_wrap">
-              {content}
-            </div>
-            <form 
-              className='replies_form'
-              onSubmit={onsubmit}
-            >
-              <textarea
-                type="text"
-                placeholder="what is your reply?"
-                value={message}
-                onChange={textarea_onchange} 
-              />
-              <div className='submit'>
-                <div className={classes.join(' ')}>{240-count}</div>
-                <button type='submit'>Reply</button>
-              </div>
-            </form>
-          </div>
-        </div>
+  return (
+    <form 
+      className='message_form'
+      onSubmit={onsubmit}
+    >
+      <textarea
+        type="text"
+        placeholder="send a direct message..."
+        value={message}
+        onChange={textarea_onchange} 
+      />
+      <div className='submit'>
+        <div className={classes.join(' ')}>{1024-count}</div>
+        <button type='submit'>Message</button>
       </div>
-    );
-  }
+    </form>
+  );
 }
